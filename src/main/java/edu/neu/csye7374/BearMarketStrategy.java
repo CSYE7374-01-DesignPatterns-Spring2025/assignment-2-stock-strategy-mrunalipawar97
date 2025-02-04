282c4530b824d3f71587c2faddd6733ab7253714
mrunalipawar97 <pawar.mr@northeastern.edu>
2025-02-04 17:26:43 -0500

    bear strategy

--- src/main/java/edu/neu/csye7374/BearMarketStrategy.java
+++ src/main/java/edu/neu/csye7374/BearMarketStrategy.java
@@ -8,11 +8,7 @@ public class BearMarketStrategy implements StockPriceStrategyAPI{
 	@Override
 	public double computeNewStockPrice(double currentPrice, double bid) {
 		double discountRate = 0.90; //Decrease by 10%
-		double newPrice = currentPrice + (bid - currentPrice) * discountRate;
-		if (newPrice < 50) {  // Minimum price threshold
-	        newPrice = 50;
-	    }
-		return newPrice ;
+		return currentPrice + (bid - currentPrice) * discountRate;
 		
 	}
 
